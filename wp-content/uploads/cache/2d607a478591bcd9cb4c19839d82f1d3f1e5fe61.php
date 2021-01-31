9caa3cd0ef23d61095b1c0583a3cf11e94c308ea
Lauri Himanen <lauri.himanen@gmail.com>
2021-01-31 21:51:10 +0200

    Initial commit

---
+++ wp-content/uploads/cache/2d607a478591bcd9cb4c19839d82f1d3f1e5fe61.php
@@ -0,0 +1,11 @@
+<header class="banner">
+  <div class="container">
+    <div id="menubar">
+        <div id="left-side">
+        </div>
+        <div class="horizontal-spacer"></div>
+        <div id="menu-toggle">
+        <i id="menu-close" class="material-icons">close</i>
+        <i id="menu-open" class="material-icons">menu</i>
+        </div>
+        <nav class="nav-primary">
